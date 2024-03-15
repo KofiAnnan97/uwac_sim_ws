599c7deed9bd1740eff5e3bd0408fb72b13a9e90
Elinam Gbordzoe <kofi4annan@gmail.com>
2024-03-15 00:14:24 -0400

    Added command line to reporting script.

--- ekg_auv_testing/src/report.py
+++ ekg_auv_testing/src/report.py
@@ -1,43 +1,57 @@
-import rospkg
 import numpy as np
-import math
-import csv
-import os
-import matplotlib.pyplot as plt
+import argparse
 
 from Graphing3D import PerformanceMetrics, Graphing3D
 
-title = "Helical Glide Test"
-  
-time_flag = True
-g3d = Graphing3D(time_flag)
-
 #labels = ["true_pose", "dead_reckoning", "avg", "weighted_avg", "closest_neighbor"]
 labels = ["true_pose", "dead_reckoning", "weighted_avg", "weighted_avg_with_dead_reckoning"]
-timestamp = "2024-03-09_23:26:18"
+#labels = ["true_pose","wavg_15","wavg_10","wavg_5","wavg_30"]
 
-pm = PerformanceMetrics()
+default_filename = "Helical Glide Test"
+#timestamp = "2024-03-14_23:29:08"
 
-g3d.get_data_from_csv(labels, timestamp)
-data_pos = []
-data_x  = []
-data_y = []
-data_z = [] 
-ground_truth = g3d.paths[labels[0]].path
-for i in range(1,len(labels)):
-    method = g3d.paths[labels[i]].path
-    metrics = pm.get_pose_diff_metrics(ground_truth, method, labels[i])
-    data_pos.append(metrics)
-    metrics = pm.get_single_val_metrics(ground_truth, method, "x")
-    data_x.append(metrics)
-    metrics = pm.get_single_val_metrics(ground_truth, method, "y")
-    data_y.append(metrics)
-    metrics = pm.get_single_val_metrics(ground_truth, method, "z")
-    data_z.append(metrics)
 
-collection = [data_pos, data_x, data_y, data_z]
-order = ["Position", "X", "Y", "Z"]
+def main():
+    parser = argparse.ArgumentParser(
+                        prog='ReportGenerator',
+                        description='A tool to assist in generating graphs and tables for the data collected using this ROS Package.')
+    parser.add_argument('-f', '--filename', default=default_filename, action='store', help='Provide a title for file generation.')
+    parser.add_argument('-t', '--timestamp', action='store', help="Provide a timestamp of the following format: YYYY-MM-DD_hh:mm:ss")
 
-cols = ('RSME', 'Mean', 'Standard Deviation')
-g3d.show_table(f"Relative Pose Estimation", timestamp, labels[1:], cols, collection, order)
-g3d.show_plot(title, timestamp)
+    args = parser.parse_args()
+    title = args.filename
+    timestamp = args.timestamp
+    
+    time_flag = True
+
+    pm = PerformanceMetrics()
+
+    g3d = Graphing3D(time_flag)
+    g3d.get_data_from_csv(labels, timestamp)
+    data_pos = []
+    data_x  = []
+    data_y = []
+    data_z = [] 
+    ground_truth = g3d.paths[labels[0]].path
+    for i in range(1,len(labels)):
+        method = g3d.paths[labels[i]].path
+        metrics = pm.get_pose_diff_metrics(ground_truth, method, labels[i])
+        data_pos.append(metrics)
+        metrics = pm.get_single_val_metrics(ground_truth, method, "x")
+        data_x.append(metrics)
+        metrics = pm.get_single_val_metrics(ground_truth, method, "y")
+        data_y.append(metrics)
+        metrics = pm.get_single_val_metrics(ground_truth, method, "z")
+        data_z.append(metrics)
+
+    collection = [data_pos, data_x, data_y, data_z]
+    order = ["Position", "X", "Y", "Z"]
+
+    cols = ('RSME', 'Mean', 'Standard Deviation')
+    g3d.show_table(f"Relative Pose Estimation", timestamp, labels[1:], cols, collection, order)
+    g3d.show_plot(title, timestamp)
+    print("Complete")
+
+
+if __name__ == "__main__":
+    main()
